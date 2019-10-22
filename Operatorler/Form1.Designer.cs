aac488e58ad7dbfc628163cb34cbca0ef3c2c5bf
zekierdikara <zeki_erdi_kara@hotmail.com>
2019-10-22 13:03:26 +0300

    Son işlemler

--- Operatorler/Form1.Designer.cs
+++ Operatorler/Form1.Designer.cs
@@ -28,13 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnToplama = new System.Windows.Forms.Button();
+            this.btnCikartma = new System.Windows.Forms.Button();
+            this.btnCarpma = new System.Windows.Forms.Button();
+            this.btnBolme = new System.Windows.Forms.Button();
+            this.btnMod = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnToplama
+            // 
+            this.btnToplama.Location = new System.Drawing.Point(12, 12);
+            this.btnToplama.Name = "btnToplama";
+            this.btnToplama.Size = new System.Drawing.Size(235, 43);
+            this.btnToplama.TabIndex = 0;
+            this.btnToplama.Text = "Toplama";
+            this.btnToplama.UseVisualStyleBackColor = true;
+            this.btnToplama.Click += new System.EventHandler(this.BtnToplama_Click);
+            // 
+            // btnCikartma
+            // 
+            this.btnCikartma.Location = new System.Drawing.Point(12, 61);
+            this.btnCikartma.Name = "btnCikartma";
+            this.btnCikartma.Size = new System.Drawing.Size(235, 45);
+            this.btnCikartma.TabIndex = 1;
+            this.btnCikartma.Text = "Çıkartma";
+            this.btnCikartma.UseVisualStyleBackColor = true;
+            this.btnCikartma.Click += new System.EventHandler(this.BtnCikartma_Click);
+            // 
+            // btnCarpma
+            // 
+            this.btnCarpma.Location = new System.Drawing.Point(12, 112);
+            this.btnCarpma.Name = "btnCarpma";
+            this.btnCarpma.Size = new System.Drawing.Size(235, 48);
+            this.btnCarpma.TabIndex = 2;
+            this.btnCarpma.Text = "Çarpma";
+            this.btnCarpma.UseVisualStyleBackColor = true;
+            this.btnCarpma.Click += new System.EventHandler(this.BtnCarpma_Click);
+            // 
+            // btnBolme
+            // 
+            this.btnBolme.Location = new System.Drawing.Point(12, 166);
+            this.btnBolme.Name = "btnBolme";
+            this.btnBolme.Size = new System.Drawing.Size(235, 44);
+            this.btnBolme.TabIndex = 3;
+            this.btnBolme.Text = "Bölme";
+            this.btnBolme.UseVisualStyleBackColor = true;
+            this.btnBolme.Click += new System.EventHandler(this.BtnBolme_Click);
+            // 
+            // btnMod
+            // 
+            this.btnMod.Location = new System.Drawing.Point(12, 216);
+            this.btnMod.Name = "btnMod";
+            this.btnMod.Size = new System.Drawing.Size(235, 50);
+            this.btnMod.TabIndex = 4;
+            this.btnMod.Text = "Mod";
+            this.btnMod.UseVisualStyleBackColor = true;
+            this.btnMod.Click += new System.EventHandler(this.BtnMod_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(266, 325);
+            this.Controls.Add(this.btnMod);
+            this.Controls.Add(this.btnBolme);
+            this.Controls.Add(this.btnCarpma);
+            this.Controls.Add(this.btnCikartma);
+            this.Controls.Add(this.btnToplama);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnToplama;
+        private System.Windows.Forms.Button btnCikartma;
+        private System.Windows.Forms.Button btnCarpma;
+        private System.Windows.Forms.Button btnBolme;
+        private System.Windows.Forms.Button btnMod;
     }
 }
 
--- Operatorler/Form1.cs
+++ Operatorler/Form1.cs
@@ -16,5 +16,52 @@ namespace Operatorler
         {
             InitializeComponent();
         }
+
+        private void BtnToplama_Click(object sender, EventArgs e)
+        {
+            int s1 = 120;
+            int s2 = 25;
+
+            int toplam = s1 + s2;
+            // MessageBox.Show("Toplama işleminin sonucu : " + toplam);
+            MessageBox.Show("Toplama işleminin sonucu : " +(s1 + s2)); // Çıktı =>145
+        }
+
+        private void BtnCikartma_Click(object sender, EventArgs e)
+        {
+            int s1 = 120;
+            int s2 = 25;
+
+            int cikarma = s1 - s2;
+            MessageBox.Show("cikarma işleminin sonucu : " + cikarma);
+        }
+
+        private void BtnCarpma_Click(object sender, EventArgs e)
+        {
+            long s1 = 168;
+            long s2 = 150;
+
+            long carpma = s1 * s2;
+            MessageBox.Show("Carpma işleminin sonucu : " + carpma);
+        }
+
+        private void BtnBolme_Click(object sender, EventArgs e)
+        {
+            double s1 = 3448;
+            double s2 = 25;
+
+            double bölme = s1 / s2;
+            MessageBox.Show("Bölme işleminin sonucu : " + bölme);
+        }
+
+        private void BtnMod_Click(object sender, EventArgs e)
+        {
+            float s1 = 28;
+            float s2 = 11.5F;
+
+            float mod = s1 % s2;
+            MessageBox.Show("Mod işleminin sonucu : " + mod);
+
+        }
     }
 }
